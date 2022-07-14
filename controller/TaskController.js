3be96325a74543d46a135850ba76001056c8c459
guihenriquedev <guilherme_3507@hotmail.com>
2022-07-14 14:58:58 -0300

    getbyid,update and delete

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -1,28 +1,62 @@
-const Task = require('../models/Task');
+const Task = require("../models/Task");
 
 const getAllTask = async (req, res) => {
-   try{
+  try {
     const taskList = await Task.find();
-    return res.render("index", {taskList});
-   } catch(error){
-    res.status(500).send({error: error.message})
-   }
+    return res.render("index", { taskList, task: null, taskDelete: null });
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+};
+
+const createTask = async (req, res) => {
+  const task = req.body;
+
+  if (!task) {
+    return res.redirect("/");
+  }
+
+  try {
+    await Task.create(task);
+    return res.redirect("/");
+  } catch (error) {
+    res.status(500).send({ error: error.message });
+  }
+};
+
+const getById = async (req, res) => {
+  try{
+    const taskList = await Task.find();
+    if (req.params.method == "update"){
+      const task = await Task.findOne({ _id: req.params.id });
+      res.render("index", { task, taskDelete: null, taskList });
+    }else{
+      const taskDelete = await Task.findOne({ _id: req.params.id });
+      res.render("index", {task: null, taskDelete, taskList });
+    }
+  }catch(error){
+    res.status(500).send({ error: error.message });
+  }
 
 };
 
-const createTask = async (req,res) => {
-  const task = req.body;
-
-  if(!task){
-    return res.redirect("/")
-  }
-
+const update = async (req, res) => {
   try{
-    await Task.create(task)
-    return res.redirect("/")
+    const task = req.body
+    await Task.updateOne({_id: req.params.id}, task)
+    res.redirect("/")
   }catch(error){
-    res.status(500).send({error:error.message})
+    res.status(500).send({ error: error.message });
   }
-  
 }
-module.exports = { getAllTask, createTask };
+
+const deleteTask = async (req,res) =>{
+  try{
+    await Task.deleteOne ({_id: req.params.id})
+    res.redirect("/")
+  } catch(error){
+    res.status(500).send({error: error.message})
+  }
+}
+
+module.exports = { getAllTask, createTask, getById, update,deleteTask };
--- routes/routes.js
+++ routes/routes.js
@@ -3,5 +3,8 @@ const TaskController = require("../controller/TaskController")
 
 router.get("/",TaskController.getAllTask)
 router.post("/create", TaskController.createTask)
+router.get("/getByID/:id/:method", TaskController.getById)
+router.post("/update/:id", TaskController.update)
+router.get("/deleteTask/:id", TaskController.deleteTask)
 
 module.exports = router
