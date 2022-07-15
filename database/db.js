65a31227fe246d9d5987d86931f7705c483ac213
guihenriquedev <guilherme_3507@hotmail.com>
2022-07-15 11:15:33 -0300

    deploy config

--- database/db.js
+++ database/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectToDb = () => {
   mongoose.connect(
-    "mongodb+srv://banco:admin@cluster.9izw2.mongodb.net/?retryWrites=true&w=majority",
+    process.env.DB_URI,
     { useNewUrlParser: true, useUnifiedTopology: true }
   ).then(() => {
     console.log("Conectamos ao Servidor :)");
